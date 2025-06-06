348e4597282d8c8bccfed023aa8e07cca666d9e2
Namagemx <106352841+Namagemx@users.noreply.github.com>
2025-06-05 19:14:49 -0500

    added meta tags

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Main from "./Component/Main";
+import Header from "./Component/Header";
+import Nav from "./Component/Nav";
+import Footer from "./Component/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <img src={""} alt="" />
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Nav;
