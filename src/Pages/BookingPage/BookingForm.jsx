c33aa2fe0a59419df65babf6e161aa27f260ee21
Namagemx <106352841+Namagemx@users.noreply.github.com>
2025-06-13 14:09:28 -0500

    replace useState with useReducer for Booking times

--- src/Pages/BookingPage/BookingForm.jsx
+++ src/Pages/BookingPage/BookingForm.jsx
@@ -1,31 +1,19 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
 import "./../../App.css";
 
-const BookingForm = () => {
-  const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-  const [data, setData] = useState({
-    date: "",
-    time: "",
-    guest: "1",
-    occasion: "Birthday",
-  });
-
+const BookingForm = (props) => {
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    props.dispatch({ type: "SET_TIME", field: name, value });
   };
-
   const submitForm = (e) => {
     e.preventDefault();
-    console.log("Form Submitted", data);
-    data;
+    console.log("Form Submitted", props.state);
+    props.dispatch({ type: "RESET" });
   };
 
   return (
-    <Fragment>
+    <>
       <div className="booking-container">
         <h1>Book A Table</h1>
         <form onSubmit={submitForm}>
@@ -34,7 +22,7 @@ const BookingForm = () => {
             <input
               name="date"
               type="date"
-              value={data.date}
+              value={props.state.date}
               onChange={handleChange}
               id="res-date"
             />
@@ -43,37 +31,38 @@ const BookingForm = () => {
             <label htmlFor="res-time">Choose time</label>
             <select
               name="time"
-              value={data.time}
+              value={props.state.time}
               onChange={handleChange}
               id="res-time"
             >
-              {availableTimes.map((time, index) => (
-                <option key={index} value={time}>
-                  {time}
-                </option>
-              ))}
+              <option value="">Select time</option>
+              <option value="17:00">17:00</option>
+              <option value="18:00">18:00</option>
+              <option value="19:00">19:00</option>
+              <option value="20:00">20:00</option>
+              <option value="21:00">21:00</option>
             </select>
           </div>
           <div className="booking-div">
-            <label htmlFor="guests">Number of guests</label>
+            <label htmlFor="res-guests">Number of guests</label>
             <input
-              name="guest"
+              name="guests"
               type="number"
-              value={data.guest}
+              value={props.state.guests}
               onChange={handleChange}
               placeholder="1"
               min="1"
               max="10"
-              id="guests"
+              id="res-guests"
             />
           </div>
           <div className="booking-div">
-            <label htmlFor="occasion">Occasion</label>
+            <label htmlFor="res-occasion">Occasion</label>
             <select
               name="occasion"
-              value={data.occasion}
+              value={props.state.occasion}
               onChange={handleChange}
-              id="occasion"
+              id="res-occasion"
             >
               <option value="Birthday">Birthday</option>
               <option value="Anniversary">Anniversary</option>
@@ -82,7 +71,7 @@ const BookingForm = () => {
           <button type="submit">Make Your Reservation</button>
         </form>
       </div>
-    </Fragment>
+    </>
   );
 };
 
--- src/Pages/BookingPage/BookingPage.jsx
+++ src/Pages/BookingPage/BookingPage.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useReducer } from "react";
 import BookingForm from "./BookingForm";
 const BookingPage = () => {
+  const initializedTimes = {
+    date: "",
+    time: "",
+    guests: "1",
+    occassion: "Birthday",
+  };
+  function updateTimes(state, action) {
+    switch (action.type) {
+      case "SET_TIME":
+        return { ...state, [action.field]: action.value };
+      case "RESET":
+        return initializedTimes;
+      default:
+        return state;
+    }
+  }
+  const [state, dispatch] = useReducer(updateTimes, initializedTimes);
   return (
     <div>
-      <BookingForm />
+      <BookingForm state={state} dispatch={dispatch} />
     </div>
   );
 };
